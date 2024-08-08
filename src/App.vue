2cc5bed48685dd9496d86ad2a423f584dfa430a4
Vic428-human <chiaochiao1106@gmail.com>
2024-08-08 10:21:55 +0800

    對子組件props whattosay

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <PersonComponent msg="準備開始模擬table filter 的功能"/>
+    <PersonComponent :whatToSay="`準備開始模擬table filter 的功能`"></PersonComponent>
     <!-- <DataTable msg="準備開始模擬table filter 的功能"/> -->
   </div>
 </template>
