d4c2a1abdcbbe4c829d210f3c00b6916ae186b79
Vic428-human <chiaochiao1106@gmail.com>
2024-08-08 10:01:14 +0800

    改寫原先組件的做法

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="準備開始模擬table filter 的功能"/>
+    <PersonComponent msg="準備開始模擬table filter 的功能"/>
+    <!-- <DataTable msg="準備開始模擬table filter 的功能"/> -->
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+// import DataTable from "./components/DataTable.vue";
+import PersonComponent from "./components/PersonComponent.vue";
 
-@Component({
+export default {
   components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+    PersonComponent
+  }
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
